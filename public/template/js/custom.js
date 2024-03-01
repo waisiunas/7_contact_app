280def8ccc5a45dd2936e7d65eb3abcd30c1e684
Awais Younis <68760826+waisiunas@users.noreply.github.com>
2024-03-01 11:46:11 +0500

    Update custom.js

--- public/template/js/custom.js
+++ public/template/js/custom.js
@@ -1,8 +1,9 @@
+showCategories();
+
 const addFormElement = document.querySelector("#add-form");
 const addAlertElement = document.querySelector("#add-alert");
 
-
-addFormElement.addEventListener('submit', async function (e) {
+addFormElement.addEventListener("submit", async function (e) {
     e.preventDefault();
 
     const addNameElement = document.querySelector("#add-name");
@@ -10,10 +11,10 @@ addFormElement.addEventListener('submit', async function (e) {
     let addNameValue = addNameElement.value;
 
     if (addNameValue == "") {
-        addNameElement.classList.add('is-invalid');
+        addNameElement.classList.add("is-invalid");
         addAlertElement.innerHTML = alert("Provide category name!");
     } else {
-        addNameElement.classList.remove('is-invalid');
+        addNameElement.classList.remove("is-invalid");
         addAlertElement.innerHTML = "";
 
         const token = document.querySelector('input[name="_token"]').value;
@@ -25,21 +26,21 @@ addFormElement.addEventListener('submit', async function (e) {
         };
 
         const response = await fetch(addRoute, {
-            method: 'POST',
+            method: "POST",
             body: JSON.stringify(data),
             headers: {
-                'Content-Type': 'application/json'
-            }
+                "Content-Type": "application/json",
+            },
         });
 
         const result = await response.json();
 
         if (result.errors) {
             if (result.errors.name) {
-                addNameElement.classList.add('is-invalid');
+                addNameElement.classList.add("is-invalid");
                 addAlertElement.innerHTML = alert(result.errors.name);
             } else {
-                addNameElement.classList.remove('is-invalid');
+                addNameElement.classList.remove("is-invalid");
                 addAlertElement.innerHTML = "";
             }
         } else if (result.success) {
@@ -50,7 +51,123 @@ addFormElement.addEventListener('submit', async function (e) {
         } else {
             addAlertElement.innerHTML = alert();
         }
+    }
+});
 
+async function showCategories() {
+    const response = await fetch(showAllRoute);
+
+    const result = await response.json();
+
+    const responseElement = document.querySelector("#response");
+
+    if (result.categories.length > 1) {
+        let rows = "";
+
+        result.categories.forEach(function (category, index) {
+            rows += `<tr>
+            <td>${index + 1}</td>
+            <td>${category.name}</td>
+            <td>
+                <button type="button" class="btn btn-primary" onclick="editCategory(${
+                    category.id
+                })" data-bs-toggle="modal"
+                    data-bs-target="#editModal">
+                    Edit
+                </button>
+
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                    data-bs-target="#deleteModal">
+                    Delete
+                </button>
+            </td>
+        </tr>`;
+        });
+
+        responseElement.innerHTML = `<table class="table table-bordered m-0">
+        <thead>
+            <tr>
+                <th>Sr. No.</th>
+                <th>Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        ${rows}
+        </tbody>
+    </table>`;
+    } else {
+        responseElement.innerHTML = `<div class="alert alert-info m-0">
+        No record found!
+    </div>`;
+    }
+}
+
+let mainId = 0;
+
+async function editCategory(id) {
+    mainId = id;
+    const apiURL = showSingleRoute.replace(":id", id);
+    const response = await fetch(apiURL);
+    const result = await response.json();
+
+    document.querySelector("#edit-name").value = result.category.name;
+}
+
+const editFormElement = document.querySelector("#edit-form");
+const editAlertElement = document.querySelector("#edit-alert");
+
+editFormElement.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const editNameElement = document.querySelector("#edit-name");
+
+    let editNameValue = editNameElement.value;
+
+    if (editNameValue == "") {
+        editNameElement.classList.add("is-invalid");
+        editAlertElement.innerHTML = alert("Provide category name!");
+    } else {
+        editNameElement.classList.remove("is-invalid");
+        editAlertElement.innerHTML = "";
+
+        const token = document.querySelector('input[name="_token"]').value;
+
+        const data = {
+            name: editNameValue,
+            id: ID,
+            _token: token,
+        };
+
+        const apiURL = editRoute.replace(":id", mainId);
+
+        const response = await fetch(apiURL, {
+            method: "PATCH",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const result = await response.json();
+
+        if (result.errors) {
+            if (result.errors.name) {
+                editNameElement.classList.add("is-invalid");
+                editAlertElement.innerHTML = alert(result.errors.name);
+            } else {
+                editNameElement.classList.remove("is-invalid");
+                editAlertElement.innerHTML = "";
+            }
+        } else if (result.success) {
+            editAlertElement.innerHTML = alert(result.success, "success");
+            showCategories();
+        } else if (result.failure) {
+            editAlertElement.innerHTML = alert(result.failure);
+        } else {
+            editAlertElement.innerHTML = alert();
+        }
     }
 });
 
